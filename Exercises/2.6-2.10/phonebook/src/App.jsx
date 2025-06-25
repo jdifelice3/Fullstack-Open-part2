c28b43ca8fee5044c05e3af0700a097e4cf7d912
Your Name <johndifelice@protonmail.com>
2025-06-25 17:46:33 -0400

    Exercises: 2.1-2.5, 2.6-2.10, 2.11

---
+++ Exercises/2.6-2.10/phonebook/src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import {Person, PersonList} from './components/Person.jsx';
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [filteredPersons, setFilteredPersons] = useState([])
+  const [noSearchResults, setNoSearchResults] = useState(false);
+
+  const submitOnClick = (event) => {
+    event.preventDefault();
+    console.log('persons', persons);
+    
+    console.log('button clicked', event.target)
+    const name = document.getElementById('name').value;
+    console.log('name', name);
+
+    if(!name || name.length === 0) {
+      alert('Name cannot be empty')
+      return
+    }
+
+    if(persons.find(p => p.name === name)) {
+      alert(`${name} is already added to phonebook`)
+      return
+    } else {
+      setPersons(persons.concat({ name, number: document.getElementById('number').value }));
+      setNewName(name);
+    }
+  }
+
+  const filterOnChange = (event) => {
+    console.log('In filterOnChange');
+    const filter = event.target.value;
+    console.log('filter', filter);
+    if(!filter || filter.length === 0) {
+      console.log('filter is empty');
+      setFilteredPersons([]);
+      setNoSearchResults(false);
+      return;
+    }
+    const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()));
+    console.log('filteredPersons', filteredPersons);
+    
+    if(filter.length > 0 && filteredPersons.length === 0) {
+      setFilteredPersons([]);
+      setNoSearchResults(true);
+      return;
+    }
+    setFilteredPersons(filteredPersons);
+  }
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <div>filter shown with <input id="filter" onChange={filterOnChange}/></div>
+      
+      <div><h2>add a new</h2></div>
+      <form>
+        <div>name: <input id="name"/></div>
+        <div>number: <input id="number"/></div>
+        <div><button type="submit" onClick={submitOnClick}>add</button></div>
+      </form>
+      <h2>Numbers</h2>
+      <PersonList persons={persons} filteredPersons={filteredPersons} noSearchResults={noSearchResults} />
+    </div>
+  )
+}
+
+export default App
